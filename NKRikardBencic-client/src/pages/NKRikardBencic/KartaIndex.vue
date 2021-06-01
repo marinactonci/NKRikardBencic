659ba68cf3ae070ffe367ded3871da41425de6f0
Tonci Marinac <toncimarinac@gmail.com>
2021-06-01 12:19:03 +0200

    Nova dorada KartaIndex.vue

--- NKRikardBencic-client/src/pages/NKRikardBencic/KartaIndex.vue
+++ NKRikardBencic-client/src/pages/NKRikardBencic/KartaIndex.vue
@@ -1,89 +1,71 @@
 <template>
-    <div class="q-pa-lg row justify-center">
-        <div
-            class="col"
-            style="max-width: 500px"
-        >
-            <q-table
-                v-if="isMounted"
-                title="Karta"
-                :data="kartas"
-                :columns="columns"
-                row-key="UIDKarta"
-                flat
-            >
-                <template v-slot:top>
-                    <q-btn
-                        color="primary"
-                        label="Dodaj"
-                        @click="onNewRow"
-                    />
-                </template>
-                <template v-slot:body-cell="props">
-                    <q-td :props="props">
-                        <div v-if="props.col.name==='actions'">
-                            <q-btn
-                                dense
-                                color="primary"
-                                icon="edit"
-                                @click="onUpdateRow(props.row)"
-                            />
-                            <q-btn
-                                dense
-                                color="primary"
-                                icon="delete"
-                                @click="onDeleteRow(props.row)"
-                            />
-                        </div>
-                        <div v-else>
-                            {{props.row[props.col.field]}}
-                        </div>
-                    </q-td>
-                </template>
-            </q-table>
-        </div>
-        <q-dialog
-            v-if="openDialog"
-            v-model="openDialog"
-        >
-            <q-card class="q-dialog-plugin">
-                <q-card-section>
-                    <div class="text-h6">
-                        Karta
-                    </div>
-                </q-card-section>
-                <q-separator />
-                <q-card-section>
-                    <q-input
-                        ref="Datum"
-                        :error="!karta.datum || karta.datum.lenght === 0"
-                        error-message="Datum je obavezan."
-                        label="Datum"
-                        v-model="karta.datum"
-                    />
-                    <q-input
-                        ref="Cijena"
-                        :error="!karta.cijena || karta.datum.cijena === 0"
-                        error-message="Cijena je obavezna."
-                        label="Cijena"
-                        v-model="karta.cijena"
-                    />
-                </q-card-section>
-                <q-card-actions align="right">
-                    <q-btn
-                        color="primary"
-                        label="OK"
-                        @click="onOKClick"
-                    />
-                    <q-btn
-                        color="primary"
-                        label="Cancel"
-                        @click="onCancelClick"
-                    />
-                </q-card-actions>
-            </q-card>
-        </q-dialog>
+  <div class="q-pa-lg row justify-center">
+    <div class="col" style="max-width: 500px">
+      <q-table
+        v-if="isMounted"
+        title="kartas"
+        :data="kartas"
+        :columns="columns"
+        row-key="UIDkarta"
+        flat
+      >
+        <template v-slot:top>
+          <q-btn color="primary" label="Add karta" @click="onNewRow" />
+        </template>
+        <template v-slot:body-cell="props">
+          <q-td :props="props">
+            <div v-if="props.col.name === 'actions'">
+              <q-btn
+                dense
+                color="primary"
+                icon="edit"
+                @click="onUpdateRow(props.row)"
+              />
+              <q-btn
+                dense
+                color="primary"
+                icon="delete"
+                @click="onDeleteRow(props.row)"
+              />
+            </div>
+            <div v-else>
+              {{ props.row[props.col.field] }}
+            </div>
+          </q-td>
+        </template>
+      </q-table>
     </div>
+    <q-dialog v-if="openDialog" v-model="openDialog">
+      <q-card class="q-dialog-plugin">
+        <q-card-section>
+          <div class="text-h6">
+            Karta
+          </div>
+        </q-card-section>
+        <q-separator />
+        <q-card-section>
+          <q-input
+            ref="Datum"
+            :error="!karta.datum || karta.datum.length === 0"
+            error-message="Datum is required."
+            label="Datum"
+            v-model="karta.datum"
+          />
+          <q-input
+            ref="Cijena"
+            :error="!karta.cijena || karta.datum.cijena === 0"
+            error-message="Cijena is required."
+            label="Cijena"
+            v-model="karta.cijena"
+          />
+        </q-card-section>
+        <q-card-actions align="right">
+          <q-btn color="primary" label="OK" @click="onOKClick" />
+          <q-btn color="primary" label="Cancel" @click="onCancelClick" />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+  </div>
 </template>
 <script>
 export default {
@@ -93,7 +75,7 @@ export default {
       openDialog: false,
       karta: null,
       kartaModel: {
-        UIDKarta: null,
+        UIDkarta: null,
         datum: null,
         cijena: null
       },
@@ -126,13 +108,12 @@ export default {
   },
   mounted: function () {
     const collectionRef = this.$db.collection('karta')
-    collectionRef.get()
-      .then((rows) => {
-        rows.forEach((row) => {
-          this.kartas.push(row.data())
-        })
-        this.isMounted = true
+    collectionRef.get().then(rows => {
+      rows.forEach(row => {
+        this.kartas.push(row.data())
       })
+      this.isMounted = true
+    })
   },
   methods: {
     onNewRow () {
@@ -142,14 +123,16 @@ export default {
     onOKClick () {
       if (!this.$refs.datum.hasError && !this.$refs.Cijena.hasError) {
         const collectionRef = this.$db.collection('karta')
-        if (this.karta.UIDKarta === null) {
-          collectionRef.add(this.karta)
-            .then((doc) => {
-              this.karta.UIDKarta = doc.id
+        if (this.karta.UIDkarta === null) {
+          collectionRef
+            .add(this.karta)
+            .then(doc => {
+              this.karta.UIDkarta = doc.id
               const docRef = this.$db.collection('karta').doc(doc.id)
-              docRef.update({ UIDKarta: doc.id })
-                .then((response) => {
-                  this.karta.UIDKarta = doc.id
+              docRef
+                .update({ UIDkarta: doc.id })
+                .then(response => {
+                  this.karta.UIDkarta = doc.id
                   this.kartas.push(this.karta)
                   this.openDialog = false
                 })
@@ -161,13 +144,18 @@ export default {
               console.error('Error adding document: ', error)
             })
         } else {
-          const docRef = this.$db.collection('karta').doc(this.karta.UIDKarta)
-          docRef.set(this.karta)
-            .then((response) => {
-              const karta = this.kartas.find(karta => karta.UIDKarta === this.karta.UIDKarta)
+          const docRef = this.$db.collection('karta').doc(this.karta.UIDkarta)
+          docRef
+            .set(this.karta)
+            .then(response => {
+              const karta = this.kartas.find(
+                karta => karta.UIDkarta === this.karta.UIDkarta
+              )
               if (karta) {
                 for (const attributeName in this.karta) {
-                  karta[attributeName] = JSON.parse(JSON.stringify(this.karta[attributeName]))
+                  karta[attributeName] = JSON.parse(
+                    JSON.stringify(this.karta[attributeName])
+                  )
                 }
               }
               this.openDialog = false
@@ -184,28 +172,36 @@ export default {
     onUpdateRow (karta) {
       this.karta = JSON.parse(JSON.stringify(this.kartaModel))
       for (const attributeName in this.karta) {
-        this.karta[attributeName] = JSON.parse(JSON.stringify(karta[attributeName]))
+        this.karta[attributeName] = JSON.parse(
+          JSON.stringify(karta[attributeName])
+        )
       }
       this.openDialog = true
     },
     onDeleteRow (row) {
-      this.$q.dialog({
-        title: 'Delete',
-        message: 'Confirm deletion.',
-        ok: true,
-        cancel: true
-      }).onOk(() => {
-        const docRef = this.$db.collection('karta').doc(row.UIDKarta)
-        docRef.delete()
-          .then(() => {
-            const index = this.kartas.findIndex(karta => karta.UIDKarta === row.UIDKarta)
-            if (index >= 0) {
-              this.kartas.splice(index, 1)
-            }
-          }).catch((error) => {
-            console.error('Error removing document: ', error)
-          })
-      })
+      this.$q
+        .dialog({
+          title: 'Delete',
+          message: 'Confirm deletion.',
+          ok: true,
+          cancel: true
+        })
+        .onOk(() => {
+          const docRef = this.$db.collection('karta').doc(row.UIDkarta)
+          docRef
+            .delete()
+            .then(() => {
+              const index = this.kartas.findIndex(
+                karta => karta.UIDkarta === row.UIDkarta
+              )
+              if (index >= 0) {
+                this.kartas.splice(index, 1)
+              }
+            })
+            .catch(error => {
+              console.error('Error removing document: ', error)
+            })
+        })
     }
   }
 }
